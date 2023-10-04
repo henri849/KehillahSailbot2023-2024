4773c31cebde52a5fed02fad70dc2c2c49f720cf
henri849 <hsayag2024@kehillahstudent.org>
2023-10-04 09:46:44 -0700

    removed typo

--- RadioJetson.py
+++ RadioJetson.py
@@ -3,5 +3,5 @@ import time
 radio = serial.Serial(port='COM7', baudrate=57600, timeout=5)#.01, timeout=5
 
 while True:
-    image_data = radio.readline(300)
+    image_data = radio.readline()
     time.sleep(0.1)
